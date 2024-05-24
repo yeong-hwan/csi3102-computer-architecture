0b21d93148d5b9cf75c66cbb726ef45c20dfd1fc
yeong-hwan <jghff700@naver.com>
2024-05-24 13:38:33 +0900

    feat: implement memory

--- assn3/Memory.h
+++ assn3/Memory.h
@@ -8,82 +8,151 @@
 
 #define MEMORY_SIZE (32 * 1024 * 1024) // <-- 32-MB memory
 
-class Memory : public DigitalCircuit {
+class Memory : public DigitalCircuit
+{
 
   public:
+    enum Endianness
+    {
+        LittleEndian,
+        BigEndian
+    };
 
-    enum Endianness { LittleEndian, BigEndian };
+    Memory(const std::string &name, const Wire<32> *iAddress, const Wire<32> *iWriteData, const Wire<1> *iMemRead,
+           const Wire<1> *iMemWrite, Wire<32> *oReadData, const Endianness endianness,
+           const char *initFileName = nullptr)
+        : DigitalCircuit(name)
+    {
+        _endianness = endianness;
+        _iAddress = iAddress;
+        _iWriteData = iWriteData;
+        _oReadData = oReadData;
+        _iMemRead = iMemRead;
+        _iMemWrite = iMemWrite;
 
-    Memory(const std::string &name,
-           const Wire<32> *iAddress,
-           const Wire<32> *iWriteData,
-           const Wire<1> *iMemRead,
-           const Wire<1> *iMemWrite,
-           Wire<32> *oReadData,
-           const Endianness endianness,
-           const char *initFileName = nullptr) : DigitalCircuit(name) {
-      _endianness = endianness;
-      _iAddress = iAddress;
-      _iWriteData = iWriteData;
-      _oReadData = oReadData;
-      _iMemRead = iMemRead;
-      _iMemWrite = iMemWrite;
+        _memory = new std::bitset<8>[MEMORY_SIZE];
 
-      _memory = new std::bitset<8>[MEMORY_SIZE];
-
-      if (initFileName != nullptr) {
-        // Each line of the memory initialization file consists of:
-        //   - the starting memory address of a 32-bit data in hexadecimal value
-        //   - the eight-digit hexadecimal value of the data
-        // e.g., "1000 ABCD1234" stores 0xABCD1234 from 0x1000 to 0x1003.
-        FILE *initFile = fopen(initFileName, "r");
-        assert(initFile != NULL);
-        std::uint32_t addr, value;
-        while (fscanf(initFile, " %x %x", &addr, &value) == 2) {
-          printf("INFO: memory[0x%08lx..0x%08lx] <-- 0x%08lx\n",
-                 (unsigned long)addr, (unsigned long)(addr + 3),
-                 (unsigned long)value);
-          if (_endianness == LittleEndian) {
-            _memory[addr + 0] = (std::uint8_t)(value % 0x100); value >>= 8;
-            _memory[addr + 1] = (std::uint8_t)(value % 0x100); value >>= 8;
-            _memory[addr + 2] = (std::uint8_t)(value % 0x100); value >>= 8;
-            _memory[addr + 3] = (std::uint8_t)(value % 0x100);
-          } else { // _endianness == BigEndian
-            _memory[addr + 3] = (std::uint8_t)(value % 0x100); value >>= 8;
-            _memory[addr + 2] = (std::uint8_t)(value % 0x100); value >>= 8;
-            _memory[addr + 1] = (std::uint8_t)(value % 0x100); value >>= 8;
-            _memory[addr + 0] = (std::uint8_t)(value % 0x100);
-          }
+        if (initFileName != nullptr)
+        {
+            // Each line of the memory initialization file consists of:
+            //   - the starting memory address of a 32-bit data in hexadecimal value
+            //   - the eight-digit hexadecimal value of the data
+            // e.g., "1000 ABCD1234" stores 0xABCD1234 from 0x1000 to 0x1003.
+            FILE *initFile = fopen(initFileName, "r");
+            assert(initFile != NULL);
+            std::uint32_t addr, value;
+            while (fscanf(initFile, " %x %x", &addr, &value) == 2)
+            {
+                printf("INFO: memory[0x%08lx..0x%08lx] <-- 0x%08lx\n", (unsigned long)addr, (unsigned long)(addr + 3),
+                       (unsigned long)value);
+                if (_endianness == LittleEndian)
+                {
+                    _memory[addr + 0] = (std::uint8_t)(value % 0x100);
+                    value >>= 8;
+                    _memory[addr + 1] = (std::uint8_t)(value % 0x100);
+                    value >>= 8;
+                    _memory[addr + 2] = (std::uint8_t)(value % 0x100);
+                    value >>= 8;
+                    _memory[addr + 3] = (std::uint8_t)(value % 0x100);
+                }
+                else
+                { // _endianness == BigEndian
+                    _memory[addr + 3] = (std::uint8_t)(value % 0x100);
+                    value >>= 8;
+                    _memory[addr + 2] = (std::uint8_t)(value % 0x100);
+                    value >>= 8;
+                    _memory[addr + 1] = (std::uint8_t)(value % 0x100);
+                    value >>= 8;
+                    _memory[addr + 0] = (std::uint8_t)(value % 0x100);
+                }
+            }
+            fclose(initFile);
         }
-        fclose(initFile);
-      }
     }
 
-    void printMemory() {
-      for (size_t i = 0; i < MEMORY_SIZE; i += 4) {
-        if (_memory[i].any() || _memory[i + 1].any() || _memory[i + 2].any()
-            || _memory[i + 3].any()) {
-          std::uint32_t value = 0;
-          value += (_memory[i + 3].to_ulong()); value <<= 8;
-          value += (_memory[i + 2].to_ulong()); value <<= 8;
-          value += (_memory[i + 1].to_ulong()); value <<= 8;
-          value += (_memory[i + 0].to_ulong());
-          printf("  memory[0x%08lx..0x%08lx] = 0x%08lx\n", i + 3, i,
-                 (unsigned long)value);
+    void printMemory()
+    {
+        for (size_t i = 0; i < MEMORY_SIZE; i += 4)
+        {
+            if (_memory[i].any() || _memory[i + 1].any() || _memory[i + 2].any() || _memory[i + 3].any())
+            {
+                std::uint32_t value = 0;
+                value += (_memory[i + 3].to_ulong());
+                value <<= 8;
+                value += (_memory[i + 2].to_ulong());
+                value <<= 8;
+                value += (_memory[i + 1].to_ulong());
+                value <<= 8;
+                value += (_memory[i + 0].to_ulong());
+                printf("  memory[0x%08lx..0x%08lx] = 0x%08lx\n", i + 3, i, (unsigned long)value);
+            }
         }
-      }
     }
 
-    virtual void advanceCycle() {
-      /* FIXME */
+    virtual void advanceCycle()
+    {
+        std::uint32_t address = static_cast<std::uint32_t>(_iAddress->to_ulong());
+        std::uint32_t writeData = static_cast<std::uint32_t>(_iWriteData->to_ulong());
+        bool memRead = static_cast<bool>(_iMemRead->to_ulong());
+        bool memWrite = static_cast<bool>(_iMemWrite->to_ulong());
+
+        if (memRead)
+        {
+            std::uint32_t readData = 0;
+            if (_endianness == LittleEndian)
+            {
+                readData |= _memory[address + 3].to_ulong();
+                readData <<= 8;
+                readData |= _memory[address + 2].to_ulong();
+                readData <<= 8;
+                readData |= _memory[address + 1].to_ulong();
+                readData <<= 8;
+                readData |= _memory[address + 0].to_ulong();
+            }
+            else
+            { // BigEndian
+                readData |= _memory[address + 0].to_ulong();
+                readData <<= 8;
+                readData |= _memory[address + 1].to_ulong();
+                readData <<= 8;
+                readData |= _memory[address + 2].to_ulong();
+                readData <<= 8;
+                readData |= _memory[address + 3].to_ulong();
+            }
+            *_oReadData = readData;
+        }
+
+        if (memWrite)
+        {
+            if (_endianness == LittleEndian)
+            {
+                _memory[address + 0] = (writeData & 0xFF);
+                writeData >>= 8;
+                _memory[address + 1] = (writeData & 0xFF);
+                writeData >>= 8;
+                _memory[address + 2] = (writeData & 0xFF);
+                writeData >>= 8;
+                _memory[address + 3] = (writeData & 0xFF);
+            }
+            else
+            { // BigEndian
+                _memory[address + 3] = (writeData & 0xFF);
+                writeData >>= 8;
+                _memory[address + 2] = (writeData & 0xFF);
+                writeData >>= 8;
+                _memory[address + 1] = (writeData & 0xFF);
+                writeData >>= 8;
+                _memory[address + 0] = (writeData & 0xFF);
+            }
+        }
     }
 
-    ~Memory() {
-      delete[] _memory;
+    ~Memory()
+    {
+        delete[] _memory;
     }
 
   private:
-
     const Wire<32> *_iAddress;
     const Wire<32> *_iWriteData;
     const Wire<1> *_iMemRead;
@@ -92,8 +161,6 @@ class Memory : public DigitalCircuit {
 
     Endianness _endianness;
     std::bitset<8> *_memory;
-
 };
 
 #endif
-
