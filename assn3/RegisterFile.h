bacfe47aeedb068eb9116d96b1df508fce58eb79
yeong-hwan <jghff700@naver.com>
2024-05-24 13:27:19 +0900

    feat: implement register file

--- assn3/RegisterFile.h
+++ assn3/RegisterFile.h
@@ -3,65 +3,84 @@
 
 #include "DigitalCircuit.h"
 
+#include <bitset>
 #include <cassert>
 #include <cstdio>
 
-class RegisterFile : public DigitalCircuit {
+class RegisterFile : public DigitalCircuit
+{
 
   public:
-
-    RegisterFile(const Wire<5> *iReadRegister1,
-                 const Wire<5> *iReadRegister2,
-                 const Wire<5> *iWriteRegister,
-                 const Wire<32> *iWriteData,
-                 const Wire<1> *iRegWrite,
-                 Wire<32> *oReadData1,
-                 Wire<32> *oReadData2,
+    RegisterFile(const Wire<5> *iReadRegister1, const Wire<5> *iReadRegister2, const Wire<5> *iWriteRegister,
+                 const Wire<32> *iWriteData, const Wire<1> *iRegWrite, Wire<32> *oReadData1, Wire<32> *oReadData2,
                  const char *initFileName = nullptr)
-      : DigitalCircuit("RegisterFile") {
-      _iReadRegister1 = iReadRegister1;
-      _iReadRegister2 = iReadRegister2;
-      _iWriteRegister = iWriteRegister;
-      _iWriteData = iWriteData;
-      _iRegWrite = iRegWrite;
-      _oReadData1 = oReadData1;
-      _oReadData2 = oReadData2;
+        : DigitalCircuit("RegisterFile")
+    {
+        _iReadRegister1 = iReadRegister1;
+        _iReadRegister2 = iReadRegister2;
+        _iWriteRegister = iWriteRegister;
+        _iWriteData = iWriteData;
+        _iRegWrite = iRegWrite;
+        _oReadData1 = oReadData1;
+        _oReadData2 = oReadData2;
 
-      for (size_t i = 0; i < 32; i++) {
-        _registers[i].reset();
-      }
-
-      if (initFileName != nullptr) {
-        // Each line of the memory initialization file consists of:
-        //   1) the target register index in decimal value
-        //   2) the eight-digit hexadecimal value of the data
-        // For example, "10 ABCD1234" stores 0xABCD1234 to register #10.
-        FILE *initFile = fopen(initFileName, "r");
-        assert(initFile != NULL);
-        std::uint32_t reg, val;
-        while (fscanf(initFile, " %u %x", &reg, &val) == 2) {
-          if (reg != 0) {
-            printf("INFO: $%02u <-- 0x%08lx\n", reg, (unsigned long)val);
-            _registers[reg] = val;
-          }
+        for (size_t i = 0; i < 32; i++)
+        {
+            _registers[i].reset();
         }
-      }
+
+        if (initFileName != nullptr)
+        {
+            // Each line of the memory initialization file consists of:
+            //   1) the target register index in decimal value
+            //   2) the eight-digit hexadecimal value of the data
+            // For example, "10 ABCD1234" stores 0xABCD1234 to register #10.
+            FILE *initFile = fopen(initFileName, "r");
+            assert(initFile != NULL);
+            std::uint32_t reg, val;
+            while (fscanf(initFile, " %u %x", &reg, &val) == 2)
+            {
+                if (reg != 0)
+                {
+                    printf("INFO: $%02u <-- 0x%08lx\n", reg, (unsigned long)val);
+                    _registers[reg] = val;
+                }
+            }
+        }
     }
 
-    void printRegisters() {
-      for (size_t i = 0; i < 32; i++) {
-        if (_registers[i].any()) {
-          printf("  $%02lu = 0x%08lx\n", i, _registers[i].to_ulong());
+    void printRegisters()
+    {
+        for (size_t i = 0; i < 32; i++)
+        {
+            if (_registers[i].any())
+            {
+                printf("  $%02lu = 0x%08lx\n", i, _registers[i].to_ulong());
+            }
         }
-      }
     }
 
-    virtual void advanceCycle() {
-      /* FIXME */
+    virtual void advanceCycle()
+    {
+        // Read data from the registers
+        uint32_t readReg1 = static_cast<uint32_t>(_iReadRegister1->to_ulong());
+        uint32_t readReg2 = static_cast<uint32_t>(_iReadRegister2->to_ulong());
+        uint32_t writeReg = static_cast<uint32_t>(_iWriteRegister->to_ulong());
+        uint32_t writeData = static_cast<uint32_t>(_iWriteData->to_ulong());
+        bool regWrite = static_cast<bool>(_iRegWrite->to_ulong());
+
+        // Output the data from the read registers
+        *_oReadData1 = _registers[readReg1];
+        *_oReadData2 = _registers[readReg2];
+
+        // Write data to the register if RegWrite is enabled and write register is not zero
+        if (regWrite && writeReg != 0)
+        {
+            _registers[writeReg] = writeData;
+        }
     }
 
   private:
-
     const Wire<5> *_iReadRegister1;
     const Wire<5> *_iReadRegister2;
     const Wire<5> *_iWriteRegister;
@@ -71,8 +90,6 @@ class RegisterFile : public DigitalCircuit {
     Wire<32> *_oReadData2;
 
     std::bitset<32> _registers[32];
-
 };
 
 #endif
-
